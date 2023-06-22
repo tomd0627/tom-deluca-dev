f46e2b07fa13207e02471a81238148e3abc9b839
Tom DeLuca <tomd0627@optonline.net>
2023-06-22 15:18:08 -0400

    - added checks to js to prevent console errors.

--- src/scripts/_utils.js
+++ src/scripts/_utils.js
@@ -1,40 +1,48 @@
 export const initializeUtills = () => {
   // Nav toggle on mobile
-  const body = document.body;
-  const menuToggle = document.querySelector(".header__menu-toggle");
-  const menuLinks = document.querySelectorAll(".header__nav a");
+  const header = document.querySelector(".header");
 
-  const toggleHeaderNav = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    body.classList.toggle("show-nav");
-  };
+  if (header !== null) {
+    const body = document.body;
+    const menuToggle = document.querySelector(".header__menu-toggle");
+    const menuLinks = document.querySelectorAll(".header__nav a");
 
-  const closeHeaderNav = () => {
-    body.classList.remove("show-nav");
-  };
+    const toggleHeaderNav = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      body.classList.toggle("show-nav");
+    };
 
-  menuToggle.addEventListener("click", toggleHeaderNav);
-  menuToggle.addEventListener("keyup", (e) => {
-    if (e.key === "Enter" || e.keyCode === 13) {
-      toggleHeaderNav;
-    }
-  });
+    const closeHeaderNav = () => {
+      body.classList.remove("show-nav");
+    };
 
-  menuLinks.forEach((link) => {
-    link.addEventListener("click", () => {
+    menuToggle.addEventListener("click", toggleHeaderNav);
+    menuToggle.addEventListener("keyup", (e) => {
+      if (e.key === "Enter" || e.keyCode === 13) {
+        toggleHeaderNav;
+      }
+    });
+
+    menuLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        closeHeaderNav();
+      });
+    });
+
+    body.addEventListener("click", () => {
       closeHeaderNav();
     });
-  });
-
-  body.addEventListener("click", () => {
-    closeHeaderNav();
-  });
+  }
 
   // Populate copyright year
-  const date = new Date();
-  const year = date.getFullYear();
-  const copyrightYear = document.querySelector(".footer__copyright-year");
+  const footer = document.querySelector(".footer");
 
-  copyrightYear.innerHTML = year;
+  if (footer !== null) {
+    const date = new Date();
+    const year = date.getFullYear();
+    const copyrightYear = document.querySelector(".footer__copyright-year");
+
+    copyrightYear.innerHTML = year;
+  }
 };
