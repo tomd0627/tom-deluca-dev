d67d545dfb6abec951b858ea16247fd0a9614f5b
Tom DeLuca <tomd0627@optonline.net>
2024-04-19 16:34:55 -0400

    - updated npm packages. - refactored 'Project Card' js and styles.

--- src/scripts/_projectCard.js
+++ src/scripts/_projectCard.js
@@ -1,23 +1,14 @@
 export const initializeProjectCard = () => {
-  // Handle 'flip' effect on mobile
-  const projectCardClick = () => {
-    const projectCArds = document.querySelectorAll(
-      ".project-card-list__item-container"
-    );
+  const projectCArds = document.querySelectorAll(
+    ".project-card-list__item-container"
+  );
 
-    projectCArds.forEach((card) => {
-      card.addEventListener("click", (e) => {
-        e.preventDefault();
-        card.classList.toggle("active");
-      });
+  // Disable link click
+  projectCArds.forEach((card) => {
+    card.addEventListener("click", (e) => {
+      e.preventDefault();
     });
-  };
+  });
 
-  const mq = 992;
-
-  if (window.matchMedia(`(min-width: ${mq}px`).matches) {
-    return false;
-  } else {
-    projectCardClick();
-  }
+  projectCardClick();
 };
